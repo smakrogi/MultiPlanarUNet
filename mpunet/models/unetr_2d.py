53c7f40a9967c57080bb3f9567aaad01855f60f6
Sokratis Makrogiannis <smakrogiannis@desu.edu>
2024-07-29 18:19:18 -0400

    ENH: introduced U NetR implementation in github.com/AAitorG/UNETR_2D.

--- mpunet/models/unetr_2d.py
+++ mpunet/models/unetr_2d.py
@@ -1,4 +1,5 @@
 """
+2-D U-NetR model for MP U-Net segmentation.
 """
 
 import os
@@ -6,7 +7,7 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 from math import log2
 import tensorflow as tf
-import tensorflow.keras.layers as L
+from tensorflow.keras import layers 
 from tensorflow.keras.models import Model
 
 from mpunet.logging import ScreenLogger
@@ -18,7 +19,8 @@ from tensorflow.keras.layers import Input, BatchNormalization, Cropping2D, \
                                     UpSampling2D, Reshape
 import numpy as np
 
-class Patches(L.Layer):
+
+class Patches(layers.Layer):
     # It takes a batch of images and returns a batch of patches
     def __init__(self, patch_size):
         super(Patches, self).__init__()
@@ -36,6 +38,24 @@ class Patches(L.Layer):
         patch_dims = tf.shape(patches)[-1]
         patches = tf.reshape(patches, [batch_size, -1, patch_dims])
         return patches
+
+    
+class PatchEncoder(layers.Layer):
+    # It takes patches and projects them into a `projection_dim` dimensional space, then the position embedding is added
+    def __init__(self, num_patches, projection_dim):
+        super(PatchEncoder, self).__init__()
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
+
+    
 
 class UNetR_2D(Model):
     """
@@ -141,150 +161,250 @@ class UNetR_2D(Model):
         # Log the model definition
         self.log()
 
-    def mlp(self, x):
-        x = L.Dense(self.mlp_dim, activation="gelu")(x)
-        x = L.Dropout(self.dropout_rate)(x)
-        x = L.Dense(self.hidden_dim)(x)
-        x = L.Dropout(self.dropout_rate)(x)
-        return x
-
-    def transformer_encoder(self, x):
-        skip_1 = x
-        x = L.LayerNormalization()(x)
-        x = L.MultiHeadAttention(
-            num_heads=self.num_heads, key_dim=self.hidden_dim
-        )(x, x)
-        x = L.Add()([x, skip_1])
-
-        skip_2 = x
-        x = L.LayerNormalization()(x)
-        x = self.mlp(x)
-        x = L.Add()([x, skip_2])
-
-        return x
-
-    def conv_block(self, x, num_filters, kernel_size=3):
-        x = L.Conv2D(num_filters, kernel_size=kernel_size, padding="same")(x)
-        x = L.BatchNormalization()(x)
-        x = L.ReLU()(x)
-        return x
-
-    def deconv_block(self, x, num_filters, strides=2):
-        x = L.Conv2DTranspose(num_filters, kernel_size=2, padding="same", strides=strides)(x)
-        return x
-
-    def init_model(self):
-        """ Inputs """
-        inputs = L.Input(self.img_shape) 
-        patches = Patches(self.patch_size)(inputs)
         
-        patch_shape = (self.num_patches, self.patch_size*self.patch_size*self.num_channels)
+    def mlp(self, x):
+        """
+        It takes an input tensor and returns a tensor that is the result of applying a transformer multi-layer
+        perceptron (MLP) block to the input
+    
+        Args:
+        x: The input layer.
+        hidden_units: A list of integers, the number of units for each mlp hidden layer. 
+            It defines the dimensionality of the output space at each mlp layer
+        dropout_rate: The dropout rate to use.
+    
+        Returns:
+        The output of the last layer.
+        """
+        hidden_units = [self.mlp_dim, self.hidden_dim]
+        for units in hidden_units:
+            x = layers.Dense(units, activation=tf.nn.gelu)(x)
+            x = layers.Dropout(self.dropout_rate)(x)
+        return x
 
-        """ Patch + Position Embeddings """
-        patch_embed = L.Dense(self.hidden_dim)(patches) ## (None, 256, 768)
 
-        positions = tf.range(start=0, limit=self.num_patches, delta=1) ## (256,)
-        pos_embed = L.Embedding(input_dim=self.num_patches, output_dim=self.hidden_dim)(positions) ## (256, 768)
-        x = patch_embed + pos_embed ## (None, 256, 768)
+    '''
+    UNETR_2D BLOCKS
 
-        """ Transformer Encoder """
-        skip_connection_index = [3, 6, 9, 12]
-        skip_connections = []
+    To make easier to read, same blocks described in the UNETR architecture are defined below, but using 2D operations.
+        UNETR paper:
+            https://openaccess.thecvf.com/content/WACV2022/papers/Hatamizadeh_UNETR_Transformers_for_3D_Medical_Image_Segmentation_WACV_2022_paper.pdf
+    '''
 
-        for i in range(1, self.num_layers+1, 1):
-            x = self.transformer_encoder(x)
+    
+    def basic_yellow_block(self, x, filters, activation='relu', kernel_initializer='glorot_uniform', batch_norm=True, dropout=0.0):
+        """
+        This function takes in an input tensor, applies a convolutional layer with the specified number of
+        filters, applies batch normalization, applies an activation function, and applies dropout if
+        specified.
 
-            if i in skip_connection_index:
-                skip_connections.append(x)
+        Args:
+          x: the input tensor
+          filters: the number of filters in the convolutional layer
+          activation: the activation function to use. Defaults to relu
+          kernel_initializer: This is the initializer for the kernel weights matrix (see initializers).
+                              Defaults to glorot_uniform
+          batch_norm: Whether to use batch normalization or not. Defaults to False
+          dropout: the dropout rate
 
-        """ CNN Decoder """
-        z3, z6, z9, z12 = skip_connections
+        Returns:
+          The output of the last layer in the block.
+        """
+        x = layers.Conv2D(filters, (3,3), padding = 'same', kernel_initializer = kernel_initializer)(x)
+        x = layers.BatchNormalization() (x) if batch_norm else x
+        x = layers.Activation(activation) (x)
+        x = layers.Dropout(dropout)(x) if dropout > 0.0 else x
+        return x
 
-        ## Reshaping
-        z0 = L.Reshape((self.img_shape[0], self.img_shape[1], self.num_channels))(patches)
+    
+    def up_green_block(self, x, filters, name=None):
+        """
+        This function takes in a tensor and a number of filters and returns a tensor that is the result of
+        applying a 2x2 transpose convolution with the given number of filters.
 
-        shape = (
-            self.img_shape[0]//self.patch_size,
-            self.img_shape[1]//self.patch_size,
-            self.hidden_dim
-        )
-        z3 = L.Reshape(shape)(z3)
-        z6 = L.Reshape(shape)(z6)
-        z9 = L.Reshape(shape)(z9)
-        z12 = L.Reshape(shape)(z12)
+        Args:
+          x: the input tensor
+          filters: The number of filters for the transpose convolutional layer.
+          name: The name of the layer (optional).
 
-        ## Additional layers for managing different patch sizes
-        total_upscale_factor = int(log2(self.patch_size))
-        upscale = total_upscale_factor - 4
+        Returns:
+          The output of the Conv2DTranspose layer.
+        """
+        x = layers.Conv2DTranspose(filters, (2, 2), strides=(2, 2), padding='same', name=name) (x)
+        return x
 
-        if upscale >= 1: ## Patch size 16 or greater
-            z3 = self.deconv_block(z3, z3.shape[-1], strides=2**upscale)
-            z6 = self.deconv_block(z6, z6.shape[-1], strides=2**upscale)
-            z9 = self.deconv_block(z9, z9.shape[-1], strides=2**upscale)
-            z12 = self.deconv_block(z12, z12.shape[-1], strides=2**upscale)
-            # print(z3.shape, z6.shape, z9.shape, z12.shape)
+    
+    def mid_blue_block(self, x, filters, activation='relu', kernel_initializer='glorot_uniform', batch_norm=True, dropout=0.0):
+        """
+        This function takes in an input tensor and returns an output tensor after applying a transpose convolution which upscale x2 the spatial size, 
+        and applies a convolutional layer.
 
-        if upscale < 0: ## Patch size less than 16
-            p = 2**abs(upscale)
-            z3 = L.MaxPool2D((p, p))(z3)
-            z6 = L.MaxPool2D((p, p))(z6)
-            z9 = L.MaxPool2D((p, p))(z9)
-            z12 = L.MaxPool2D((p, p))(z12)
+        Args:
+          x: the input tensor
+          filters: number of filters in the convolutional layers
+          activation: The activation function to use. Defaults to relu
+          kernel_initializer: Initializer for the convolutional kernel weights matrix (see initializers).
+                              Defaults to glorot_uniform
+          batch_norm: Whether to use batch normalization or not. Defaults to False
+          dropout: The dropout rate.
 
-        ## Decoder 1
-        x = self.deconv_block(z12, 128)
-        s = self.deconv_block(z9, 128)
-        s = self.conv_block(s, 128)
+        Returns:
+          The output of the last layer of the block.
+        """
+        x = self.up_green_block(x, filters)
+        x = self.basic_yellow_block(x, filters, activation=activation, kernel_initializer=kernel_initializer, batch_norm=batch_norm, dropout=dropout)
+        return x
+    
+    def two_yellow(self, x, filters, activation='relu', kernel_initializer='glorot_uniform', batch_norm=True, dropout=0.0):
+        """
+        This function takes in an input tensor, and returns an output tensor that is the result of
+        applying two basic yellow blocks to the input tensor.
 
-        x = L.Concatenate()([x, s])
+        Args:
+          x: the input tensor
+          filters: number of filters in the convolutional layer
+          activation: The activation function to use. Defaults to relu
+          kernel_initializer: Initializer for the kernel weights matrix (see initializers). 
+                              Defaults to glorot_uniform
+          batch_norm: Whether to use batch normalization or not. Defaults to False
+          dropout: The dropout rate.
 
-        x = self.conv_block(x, 128)
-        x = self.conv_block(x, 128)
+        Returns:
+          The output of the second basic_yellow_block.
+        """
+        x = self.basic_yellow_block(x, filters, activation=activation, kernel_initializer=kernel_initializer, batch_norm=batch_norm, dropout=dropout)
+        x = self.basic_yellow_block(x, filters, activation=activation, kernel_initializer=kernel_initializer, batch_norm=batch_norm, dropout=0.0)
+        return x
 
-        ## Decoder 2
-        x = self.deconv_block(x, 64)
+    
+    def init_model(self, 
+                   data_augmentation = None,
+                   num_filters = 16, 
+                   decoder_activation = 'relu',
+                   decoder_kernel_init = 'he_normal',
+                   ViT_hidd_mult = 3,
+                   batch_norm = True,
+                   dropout = 0.0
+                   ):
 
-        s = self.deconv_block(z6, 64)
-        s = self.conv_block(s, 64)
-        s = self.deconv_block(s, 64)
-        s = self.conv_block(s, 64)
+        """
+        UNETR architecture adapted for 2D operations. It combines a ViT with U-Net, replaces the convolutional encoder with the ViT
+        and adapt each skip connection signal to their layer's spatial dimensionality. 
 
-        x = L.Concatenate()([x, s])
-        x = self.conv_block(x, 64)
-        x = self.conv_block(x, 64)
+        Note: Unlike the original UNETR, the sigmoid activation function is used in the last convolutional layer.
 
-        ## Decoder 3
-        x = self.deconv_block(x, 32)
+        The ViT implementation is based on keras implementation:
+            https://keras.io/examples/vision/image_classification_with_vision_transformer/
+        Only code:
+            https://github.com/keras-team/keras-io/blob/master/examples/vision/image_classification_with_vision_transformer.py
 
-        s = self.deconv_block(z3, 32)
-        s = self.conv_block(s, 32)
-        s = self.deconv_block(s, 32)
-        s = self.conv_block(s, 32)
-        s = self.deconv_block(s, 32)
-        s = self.conv_block(s, 32)
+        UNETR paper:
+            https://openaccess.thecvf.com/content/WACV2022/papers/Hatamizadeh_UNETR_Transformers_for_3D_Medical_Image_Segmentation_WACV_2022_paper.pdf
 
-        x = L.Concatenate()([x, s])
-        x = self.conv_block(x, 32)
-        x = self.conv_block(x, 32)
+        Args:
+          input_shape: the shape of the input image.
+          patch_size: the size of the patches that are extracted from the input image. As an example, to use 16x16 patches, set patch_size = 16.
+                      As each layer doubles the spatial size, this value must be 2^x.
+          num_patches: number of patches to extract from the image. Take into account that each patch must be of specified patch_size.
+          projection_dim: the dimension of the embedding space.
+          transformer_layers: number of transformer encoder layers
+          num_heads: number of heads in the multi-head attention layer.
+          transformer_units: number of units in the MLP blocks.
+          data_augmentation: a function that takes an input tensor and returns an augmented tensor. 
+                             To make use of tensorflow additional data augmentation layers 
+                             (use tf layer, if multiple layers, then use sequential() and add them, 
+                             and use the resulting sequential layer)
+          num_filters: number of filters in the first UNETR's layer of the decoder. In each layer the previous number of filters is doubled. Defaults to 16.
+          num_classes: number of classes to predict. Is the number of channels in the output tensor. Defaults to 1.
+          decoder_activation: activation function for the decoder. Defaults to relu.
+          decoder_kernel_init: Initializer for the kernel weights matrix of the convolutional layers in the
+                               decoder. Defaults to he_normal
+          ViT_hidd_mult: the multiple of the transformer encoder layers from of which the skip connection signal is going to be extracted.
+                         As an example, if we have 12 transformer encoder layers, and we set ViT_hidd_mult = 3, we are going to take
+                         [1*ViT_hidd_mult, 2*ViT_hidd_mult, 3*ViT_hidd_mult] -> [Z3, Z6, Z9] encoder's signals. Defaults to 3.
+          batch_norm: whether to use batch normalization or not. Defaults to True.
+          dropout: dropout rate for the decoder (can be a list of dropout rates for each layer).
 
-        ## Decoder 4
-        x = self.deconv_block(x, 16)
+        Returns:
+          A UNETR_2D Keras model.
+        """
 
-        s = self.conv_block(z0, 16)
-        s = self.conv_block(s, 16)
+        input_shape = self.img_shape
+        patch_size = self.patch_size
+        num_patches = self.num_patches
+        projection_dim = self.hidden_dim
+        transformer_layers = self.num_layers
+        num_heads = self.num_heads
+        num_classes = self.n_classes
 
-        x = L.Concatenate()([x, s])
-        x = self.conv_block(x, 16)
-        x = self.conv_block(x, 16)
+        # ViT
+        inputs = layers.Input(shape=input_shape)
+        # Augment data.
+        augmented = data_augmentation(inputs) if data_augmentation != None else inputs
+        # Create patches.
+        patches = Patches(patch_size)(augmented)
+        # Encode patches.
+        encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+        # Hidden states
+        hidden_states_out = []
 
-        """ Output """
-        out = L.Conv2D(self.n_classes, kernel_size=1, padding="same", activation=self.out_activation)(x)
+        # Create multiple layers of the Transformer block.
+        for _ in range(transformer_layers):
+            # Layer normalization 1.
+            x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+            # Create a multi-head attention layer.
+            attention_output = layers.MultiHeadAttention(
+                num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+            )(x1, x1)
+            # Skip connection 1.
+            x2 = layers.Add()([attention_output, encoded_patches])
+            # Layer normalization 2.
+            x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+            # MLP.
+            x3 = self.mlp(x3)
+            # Skip connection 2.
+            encoded_patches = layers.Add()([x3, x2])
+            # save hidden state
+            hidden_states_out.append(encoded_patches)
+
+        # UNETR Part (bottom_up, from the bottle-neck, to the output)
+
+        total_upscale_factor = int(log2(patch_size))
+        # make a list of dropout values if needed
+        if type( dropout ) is float: 
+            dropout = [dropout,]*total_upscale_factor
+
+        # bottleneck
+        z = layers.Reshape([ input_shape[0]//patch_size, input_shape[1]//patch_size, projection_dim ])(encoded_patches) # use the encoder output (easier to try different things)
+        x = self.up_green_block(z, num_filters * (2**(total_upscale_factor-1)) )
+
+        for layer in reversed(range(1, total_upscale_factor)):
+            # skips (with blue blocks)
+            z = layers.Reshape([ input_shape[0]//patch_size, input_shape[1]//patch_size, projection_dim ])( hidden_states_out[ (ViT_hidd_mult * layer) - 1 ] )
+            for _ in range(total_upscale_factor - layer):
+                z = self.mid_blue_block(z, num_filters * (2**layer), activation=decoder_activation, kernel_initializer=decoder_kernel_init, batch_norm=batch_norm, dropout=dropout[layer])
+            # decoder
+            x = layers.concatenate([x, z])
+            x = self.two_yellow(x, num_filters * (2**(layer)), activation=decoder_activation, kernel_initializer=decoder_kernel_init, batch_norm=batch_norm, dropout=dropout[layer])
+            x = self.up_green_block(x, num_filters * (2**(layer-1)))
+
+        # first skip connection (out of transformer)
+        first_skip = self.two_yellow(augmented, num_filters, activation=decoder_activation, kernel_initializer=decoder_kernel_init, batch_norm=batch_norm, dropout=dropout[0]) 
+        x = layers.concatenate([first_skip, x])
+
+        # UNETR_2D output 
+        x = self.two_yellow(x, num_filters, activation=decoder_activation, kernel_initializer=decoder_kernel_init, batch_norm=batch_norm, dropout=dropout[0] )
+        output = layers.Conv2D( num_classes, (1, 1), activation='softmax', name="mask") (x) # semantic segmentation -- ORIGINAL: softmax
+
+        # Create the Keras model.
+        # model = Model(inputs=inputs, outputs=output)
 
         if self.flatten_output:
-            out = Reshape([self.img_shape[0]*self.img_shape[1],
-                           self.n_classes], name='flatten_output')(out)
+            output = Reshape([self.img_shape[0]*self.img_shape[1],
+                           self.n_classes], name='flatten_output')(output)
 
-        return [inputs], [out]
+        return [inputs], [output]
+
 
     def log(self):
         self.logger("UNet Model Summary\n------------------")
