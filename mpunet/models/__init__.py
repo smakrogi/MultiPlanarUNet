deb5aea20a0c8caab2430e344753d142cca4fc08
Sokratis Makrogiannis <smakrogiannis@desu.edu>
2024-12-18 12:52:58 -0500

    ENH: added SWIN Unet 2D to dictionary.

--- mpunet/models/__init__.py
+++ mpunet/models/__init__.py
@@ -4,6 +4,7 @@ from .fusion_model import FusionModel
 from .model_init import model_initializer
 from .multitask_unet2d import MultiTaskUNet2D
 from .unetr_2d import UNetR_2D
+from .swin_unet_2d import SWIN_UNet_2D
 
 # Prepare a dictionary mapping from model names to data prep. functions
 from mpunet.preprocessing import data_preparation_funcs as dpf
@@ -12,5 +13,6 @@ PREPARATION_FUNCS = {
     "UNet": dpf.prepare_for_multi_view_unet,
     "UNet3D": dpf.prepare_for_3d_unet,
     "MultiTaskUNet2D": dpf.prepare_for_multi_task_2d,
-    "UNetR_2D": dpf.prepare_for_multi_view_unet
+    "UNetR_2D": dpf.prepare_for_multi_view_unet,
+    "SWIN_UNet_2D": dpf.prepare_for_multi_view_unet
 }
